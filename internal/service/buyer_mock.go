89c79f0b16c3566af9efe0fde699fafdbbc121a7
Manuel Firman <manuel.firman@gmail.com>
2024-10-30 17:30:41 -0300

    test(buyers): service mock added

--- internal/service/buyer_mock.go
+++ internal/service/buyer_mock.go
@@ -1,4 +1,4 @@
-package repository
+package service
 
 import (
 	"github.com/manuelfirman/go-API/internal"
@@ -10,41 +10,40 @@ func NewBuyerMock() *BuyerMock {
 	return &BuyerMock{}
 }
 
-// BuyerMock is a mock type for the Buyer repository
+// BuyerMock is a mock type for the Buyer service
 type BuyerMock struct {
-	// Embed the mock type to the BuyerMock
 	mock.Mock
 }
 
-// GetAll is a mocked method of the Buyer repository that returns a mocked result
+// GetAll is a mocked method of the Buyer service that returns a mocked result
 func (m *BuyerMock) GetAll() ([]internal.Buyer, error) {
 	// Call the mocked method and return the mocked results
 	args := m.Called()
 	return args.Get(0).([]internal.Buyer), args.Error(1)
 }
 
-// Get is a mocked method of the Buyer repository that returns a mocked result
+// Get is a mocked method of the Buyer service that returns a mocked result
 func (m *BuyerMock) Get(id int) (internal.Buyer, error) {
 	// Call the mocked method and return the mocked results
 	args := m.Called(id)
 	return args.Get(0).(internal.Buyer), args.Error(1)
 }
 
-// Save is a mocked method of the Buyer repository that returns a mocked result
+// Save is a mocked method of the Buyer service that returns a mocked result
 func (m *BuyerMock) Save(buyer *internal.Buyer) error {
 	// Call the mocked method and return the mocked results
 	args := m.Called(buyer)
 	return args.Error(0)
 }
 
-// Update is a mocked method of the Buyer repository that returns a mocked result
+// Update is a mocked method of the Buyer service that returns a mocked result
 func (m *BuyerMock) Update(buyer *internal.Buyer) error {
 	// Call the mocked method and return the mocked results
 	args := m.Called(buyer)
 	return args.Error(0)
 }
 
-// Delete is a mocked method of the Buyer repository that returns a mocked result
+// Delete is a mocked method of the Buyer service that returns a mocked result
 func (m *BuyerMock) Delete(id int) error {
 	// Call the mocked method and return the mocked results
 	args := m.Called(id)
